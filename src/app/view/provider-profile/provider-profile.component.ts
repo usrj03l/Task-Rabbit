b602c5cd901381690add9118e4bcf5252ad66640
usrj03l <6rj1598@gmail.com>
2023-07-29 01:28:20 +0530

    Update provider-profile.component.ts

--- src/app/view/provider-profile/provider-profile.component.ts
+++ src/app/view/provider-profile/provider-profile.component.ts
@@ -10,11 +10,30 @@ import Swal from 'sweetalert2';
 })
 export class ProviderProfileComponent {
 
-  profile = 'https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp';
+  defaultProfile = 'https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp';
+  profile: any;
+  bio: string = "Write something about yourself";
 
-  constructor(private auth:AuthService,private http:HttpClient){ }
+  constructor(private auth: AuthService, private http: HttpClient) { }
 
-  async imageUpload(){
+  profileData: any;
+
+  ngOnInit() {
+    this.loadProfile();
+  }
+
+  async loadProfile() {
+    const id = await this.auth.getId();
+    this.http.get('http://localhost:3000/provider/getUser/' + id).subscribe(
+      data => {
+        this.profileData = data;
+        if (this.profileData.profilePic) {
+          this.profile = 'http://localhost:3000/images/' + this.profileData.profilePic?.split('\\').pop();
+        }
+      });
+  }
+
+  async imageUpload() {
     const { value: file } = await Swal.fire({
       title: 'Select image',
       input: 'file',
@@ -24,25 +43,35 @@ export class ProviderProfileComponent {
       }
     })
 
-    if(file){   
+    if (file) {
       const id = await this.auth.getId();
       const formData = new FormData();
-      formData.append('uid',String(id));
-      formData.append('image',file as File);
-      this.http.post('http://localhost:3000/provider/profilePic',formData).subscribe(
-        image =>{
+      formData.append('uid', String(id));
+      formData.append('image', file as File);
+      this.http.post('http://localhost:3000/provider/profilePic', formData).subscribe(
+        image => {
           const imgPath = image;
           this.profile = 'http://localhost:3000/images/' + imgPath;
         }
       );
     }
-
   }
 
-  async viewImage(){
-    const id = await this.auth.getId();
-    this.http.post('http://localhost:3000/provider/profilePic',id).subscribe();
+  async updateBio() {
+    const { value: text } = await Swal.fire({
+      input: 'textarea',
+      inputLabel: 'Message',
+      inputPlaceholder: 'Type your message here...',
+      inputAttributes: {
+        'aria-label': 'Type your message here'
+      },
+      showCancelButton: true
+    })
 
+    if (text) {
+      const id = await this.auth.getId();
+      this.http.post("http://localhost:3000/provider/setBio", { 'bio': text, 'uid': id }).subscribe();
+      this.bio = text
+    }
   }
-
 }

bc2fd48ce2e2e76ed133a4c47f0560205f2eb639
usrj03l <6rj1598@gmail.com>
2023-07-31 21:13:05 +0530

    Update services-info.component.ts

--- src/app/user-view/services-info/services-info.component.ts
+++ src/app/user-view/services-info/services-info.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./services-info.component.css']
 })
 export class ServicesInfoComponent {
+  hoveredIndex: number = -1;
 
+  onStarHover(index: number): void {
+    this.hoveredIndex = index;
+  }
+
+  rating(hoveredIndex: number) {
+    const rating = hoveredIndex + 1;
+    console.log(rating);
+
+  }
 }

b25ef87de4bad33f6f70fd411666a5d9e8777ce1
usrj03l <6rj1598@gmail.com>
2023-08-03 02:07:35 +0530

    .

--- src/app/view/edit-profile/edit-profile.component.ts
+++ src/app/view/edit-profile/edit-profile.component.ts
@@ -11,34 +11,30 @@ export class EditProfileComponent {
   constructor(private fb: FormBuilder) { }
 
   contactForm = this.fb.group({
-    fname: ['', [Validators.required, Validators.maxLength(64)]],
-    lname: ['', [Validators.required, Validators.maxLength(64)]],
-    email: ['', [Validators.required, Validators.email]],
+    fname: ['', [Validators.maxLength(64)]],
+    lname: ['', [Validators.maxLength(64)]],
+    email: ['', [Validators.email]],
     establishmentYear: [null,
       [
-        Validators.required,
         Validators.minLength(4),
         Validators.maxLength(4),
         Validators.pattern(/\-?\d*\.?\d{1,2}/)
       ]],
     orgName: ['', [
-      Validators.required,
       Validators.maxLength(50)
     ]],
     aadhar: ['', [
-      Validators.required,
       Validators.minLength(12),
       Validators.maxLength(12),
       Validators.pattern(/^[0-9]\d*$/)
     ]],
     pan: ['', [
-      Validators.required,
       Validators.minLength(10),
       Validators.maxLength(10),
       Validators.pattern(/^[0-9]\d*$/)
     ]],
-    openTime: ['', [Validators.required]],
-    closeTime: ['', [Validators.required]],
+    openTime: ['',],
+    closeTime: ['',],
     aboutUs: this.fb.array([]),
     fQues: this.fb.array([])
   });
@@ -67,7 +63,7 @@ export class EditProfileComponent {
     (<FormArray>this.contactForm.get('aboutUs')).removeAt(index);
   }
 
-  addFreq(){
+  addFreq() {
     const newGroup = this.fb.group({
       'question': ['', Validators.required],
       'answer': ['', Validators.required]

c1bb1e1f6a891b973f3116b20dab5a0972d5b5ec
usrj03l <6rj1598@gmail.com>
2023-08-04 15:15:10 +0530

    modified service-info

--- src/app/user-view/services-info/services-info.component.ts
+++ src/app/user-view/services-info/services-info.component.ts
@@ -9,14 +9,21 @@ import { ApiService } from 'src/app/services/api.service';
 })
 export class ServicesInfoComponent {
   hoveredIndex: number = -1;
-  providerData:any;
-  time:any
-  constructor(private api:ApiService){ }
+  providerData: any;
+  time: any = [];
+  bYears:any;
+  
+  constructor(private api: ApiService) { }
 
-  ngOnInit(){
-     this.providerData = JSON.parse(localStorage.getItem('serviceData') || ''); 
-      this.time = (this.providerData.orgDetails.openTime > 12)? 24 - this.providerData.orgDetails.openTime.split(':').shift():this.providerData.orgDetails.openTime.split(':').shift()
+  ngOnInit() {
+    this.providerData = JSON.parse(localStorage.getItem('serviceData') || '');
+    this.time[0] = this.formatTime((this.providerData.orgDetails.openTime))
+    this.time[1] = this.formatTime((this.providerData.orgDetails.closeTime))
+
+    const date = new Date();
+    const currentDate = date.getFullYear();
     
+    this.bYears = currentDate - this.providerData.establishmentYear;
   }
 
   onStarHover(index: number): void {
@@ -26,5 +33,11 @@ export class ServicesInfoComponent {
   rating(hoveredIndex: number) {
     const rating = hoveredIndex + 1;
     console.log(rating);
+  }
+
+  formatTime(timeString: string) {
+    const [hourString, minute] = timeString.split(":");
+    const hour = + hourString % 24;
+    return (hour % 12 || 12) + ":" + minute + (hour < 12 ? "AM" : "PM");
   }
 }

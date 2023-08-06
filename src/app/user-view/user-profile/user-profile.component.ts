1e4086720576499071f41c9e37473da54e4ff1b5
usrj03l <6rj1598@gmail.com>
2023-08-07 00:48:38 +0530

    .

--- src/app/user-view/user-profile/user-profile.component.ts
+++ src/app/user-view/user-profile/user-profile.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
 import Swal from 'sweetalert2';
 
@@ -12,6 +13,7 @@ export class UserProfileComponent {
   defaultProfile = 'https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp';
   profile: any;
   profileData: any;
+  unSubscribe$ = new Subject();
 
   constructor(private auth: AuthService, private http: HttpClient) { }
 
@@ -42,12 +44,17 @@ export class UserProfileComponent {
       const formData = new FormData();
       formData.append('uid', String(id));
       formData.append('image', file as File);
-      this.http.post('http://localhost:3000/user/profilePic', formData).subscribe(
+      this.http.post('http://localhost:3000/user/profilePic', formData).pipe(takeUntil(this.unSubscribe$)).subscribe(
         image => {
           const imgPath = image;
           this.profile = 'http://localhost:3000/images/' + imgPath;
         }
       );
     }
+  }
+
+  ngOnDestroy(){
+    this.unSubscribe$.next(null);
+    this.unSubscribe$.complete();
   }
 }
